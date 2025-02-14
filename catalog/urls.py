c7a8fc820cd953459afc7b3d070390c7e849e569
Russell-Y <150970166+Russell-Y@users.noreply.github.com>
2025-02-13 20:12:33 -0600

    Assign 1 GitHub

---
+++ catalog/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+from . import views
+
+
+from django.urls import path, include, re_path
+from django.views.static import serve
+from django.conf import settings
+from . import views
+
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('book_list/', views.BookListView.as_view(), name='book_list'),
+    path('book_detail/<int:pk>', views.BookDetailView.as_view(), name='book_detail'),
+    path('author_list/', views.AuthorListView.as_view(), name='author_list'),
+    path('author_detail/<int:pk>', views.AuthorDetailView.as_view(), name='author_detail'),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+
+
+]
