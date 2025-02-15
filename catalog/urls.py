02c4ce5843392a3004a99eec1d8ede5605892511
Russell-Y <150970166+Russell-Y@users.noreply.github.com>
2025-02-14 18:55:32 -0600

    Update urls.py

--- catalog/urls.py
+++ catalog/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 from django.urls import path, include, re_path
 from django.views.static import serve
 from django.conf import settings
+from . import views
 
 
 urlpatterns = [
